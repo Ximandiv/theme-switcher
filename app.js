dd4d220f19ee01276c5ca0a18f5672ef5ba9e118
Ximandiv <ximandiv@gmail.com>
2024-11-23 14:07:49 -0500

    Remembers the last theme selected.

--- app.js
+++ app.js
@@ -1,17 +1,47 @@
 'use strict';
 
+const themeKey = "dark-mode";
+const lightText = "Light";
+const darkText = "Dark";
+
+const lightClass = "light-theme";
+const darkClass = "dark-theme";
+
 const switcher = document.querySelector('.btn');
 
-switcher.addEventListener('click', function(){
-    document.body.classList.toggle('light-theme');
-    document.body.classList.toggle('dark-theme');
+const setSwitcherText = (text) => switcher.textContent = text;
 
-    const className = document.body.className;
-    if(className == "light-theme"){
-        this.textContent = "Dark";
-    } else{
-        this.textContent = "Light";
+const setDarkMode = (choice) => localStorage.setItem(themeKey, choice);
+const isDarkModeOn = () => {
+    var isDarkModeOn = localStorage.getItem(themeKey);
+
+    return isDarkModeOn === "y";
+}
+
+window.onload = function () 
+{
+    if(isDarkModeOn()){
+        switchTheme(lightText, lightClass, darkClass);
+    }else{
+        switchTheme(darkText, darkClass, lightClass);
     }
+}
 
-    console.log('current theme: ' + className);
+switcher.addEventListener('click', function(){
+    if(isDarkModeOn()){
+        switchTheme(darkText, darkClass, lightClass);
+        setDarkMode("n");
+    }
+    else{
+        switchTheme(lightText, lightClass, darkClass);
+        setDarkMode("y");
+    }
 });
+
+function switchTheme(oppositeThemeText, classOff, classOn)
+{
+    document.body.classList.remove(classOff);
+    document.body.classList.add(classOn);
+
+    setSwitcherText(oppositeThemeText)
+}
